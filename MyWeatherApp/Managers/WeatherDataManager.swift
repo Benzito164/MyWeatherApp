e93737da79f290b76c04a11c35877dd90f78014e
Beni <beni@Benis-MBP.home>
2019-02-21 13:21:05 +0000

    deleted unused methods

--- MyWeatherApp/Managers/WeatherDataManager.swift
+++ MyWeatherApp/Managers/WeatherDataManager.swift
@@ -11,13 +11,13 @@ import Alamofire
 import SwiftyJSON
 import AlamofireImage
 import SDWebImage
+import UIKit
 
 
-class WeatherDataManager  {
-    //MARK: - HELLO FOOL :) 
+class WeatherDataManager : UIViewController {
+    //MARK: - HELLO FOOL :)
     //TODO: - change the weatherTemperature to interger so that you can round up the value.
     func getWeatherForLocation(locationsArray : [String],completionHandler: @escaping ()-> Void){
-
         locationsArray.forEach { (location) in
             let params : [String: String] = ["q":location, "APPID":WeatherEndPoint.key,"units":"metric"]
             Alamofire.request(WeatherEndPoint.currentLocationWeatherUrl, method: .get, parameters: params).responseJSON { (response) in
@@ -36,6 +36,24 @@ class WeatherDataManager  {
         }
     }
     
+    func getWeatherForSpecificLocation(location : String) -> [String:Any] {
+        let params : [String: String] = ["q":location, "APPID":WeatherEndPoint.key,"units":"metric"]
+        var weatherDataInfo :[String:Any] = [:]
+        Alamofire.request(WeatherEndPoint.currentLocationWeatherUrl, method: .get, parameters: params).responseJSON { (response) in
+            let weatherData :JSON = JSON(response.result.value!)
+            let weatherIcon = self.getWeatherIcon(icon: weatherData["weather"][0]["icon"].stringValue)
+            let weatherDescription = weatherData["weather"][0]["description"].stringValue
+            let weather = weatherData["weather"][0]["main"].stringValue
+            let weatherTemperature = weatherData["main"]["temp"].stringValue
+            weatherDataInfo["locationDescription"] = weatherDescription
+            weatherDataInfo["locationIcon"] = weatherIcon
+            weatherDataInfo["locationTemperature"] = weatherTemperature
+            weatherDataInfo["weatherData"] = weather
+            print(weatherDataInfo)
+        }
+        return weatherDataInfo
+        
+    }
     func updateWeatherArrayWithData(weather: String, weatherDescription: String, weatherIcon:UIImageView,locationName: String,temperature:String,fullLocationName:String){
         let weatherInfo = CustomCollectionViewCell(weatherIcon: weatherIcon, weather: weather, weatherLocation: locationName, temperature: temperature,weatherDescription:weatherDescription,fullLocationNameText:fullLocationName)
         if savedLocation.count > 0 {
@@ -49,7 +67,7 @@ class WeatherDataManager  {
     }
     
     fileprivate func getWeatherIcon(icon: String)-> UIImageView{
-        let imageIcon = UIImageView()
+         let imageIcon = UIImageView()
          let url = WeatherEndPoint.iconUrl+icon+".png"
           imageIcon.sd_setImage(with:URL(string:url), placeholderImage: UIImage(named: "placeholder.png"))
         return imageIcon
